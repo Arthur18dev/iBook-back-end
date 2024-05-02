0fc1d323b1258210160b7177e12b8211a587e9a2
Luis Claudio Chaves <luis.claudio.meca@gmail.com>
2024-05-02 19:51:03 -0300

    Criando rotas para pesquisa de livros por categoria

--- SRC/controllers/livros.js
+++ SRC/controllers/livros.js
@@ -3,11 +3,18 @@ const livrosModel = require('../models/livrosModel')
 
 
 const Livraria = async (req, res) => {
-        const [row] = await livrosModel.Livraria(res)
+        const [row] = await livrosModel.Livraria(req)
+        return res.json(row)
+      }
+
+      const LivrariaPorCategoria = async (req, res) => {
+        const [row] = await livrosModel.LivrariaPorCategoria(req)
         return res.json(row)
       }
 
 
+
 module.exports = {
-    Livraria
+    Livraria,
+    LivrariaPorCategoria
 }
